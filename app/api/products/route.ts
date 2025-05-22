336ea4bbe89f1edf504af5e6660bd089e0488ddb
hani osama <haniosama124@gmail.com>
2025-05-22 14:42:26 +0300

    add some handleing

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import connectDb from "../../../lib/db";
+import connectDb from "@/lib/db";
 import product from "@/models/product";
 
 export async function GET(req: NextRequest) {
